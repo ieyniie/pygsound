2f3c823f316732c60da0292e6290405f487b5019
Xiao Yongxiong <xiaoyx@zhejianglab.com>
2023-08-26 07:42:54 +0000

    custom_array_rir

--- examples/custom_array.py
+++ examples/custom_array.py
@@ -1,10 +1,21 @@
 import numpy as np
 import pygsound as ps
 from wavefile import WaveWriter, Format
+import random
+import multiprocessing as mp
+from multiprocessing import Pool
+from tqdm import tqdm
 
 
-def compute_array(meshpath, src_coord, lis_coord, r, s, micarray):
-    mesh = ps.loadobj(meshpath, r, s)
+custom_array = np.array(
+[[0, 0.035, 0], [-0.0303, 0.0175, 0], [-0.0303, -0.0175, 0], [0, -0.035, 0], [0.0303, -0.0175, 0],
+    [0.0303, 0.0175, 0]])
+r = 0.5
+sound_a = np.array([0.5,0.6,0.6,0.7,0.75,0.8,0.9,0.9])
+s = 0.1
+# mesh1 = ps.loadobj("cube.obj",r,s)
+mesh2 = ps.createbox(36, 50, 8, sound_a, s)
+def compute_array(mesh, src_coord, lis_coord, micarray,num):
     ctx = ps.Context()
     ctx.diffuse_count = 20000
     ctx.specular_count = 2000
@@ -12,40 +23,59 @@ def compute_array(meshpath, src_coord, lis_coord, r, s, micarray):
     scene = ps.Scene()
     scene.setMesh(mesh)
 
-    src = ps.Source(src_coord)
-    src.radius = 0.01
-
     res = {}
-    res_buffer = []
     rate = 0
-    for offset in micarray:
-        lis = ps.Listener((offset + lis_coord).tolist())
-        lis.radius = 0.01
+    res['samples'] = []
+    for j in range(2):
+        src = ps.Source(src_coord[:,j])
+        src.radius = 0.01
+        res_buffer = []
+        for offset in micarray:
+            lis = ps.Listener((offset + lis_coord).tolist())
+            lis.radius = 0.001
 
-        res_ch = scene.computeIR([src], [lis], ctx)
-        rate = res_ch['rate']
-        sa = res_ch['samples'][0][0][0]
-        res['rate'] = rate
-        res_buffer.append(sa)
-    res['samples'] = np.zeros((len(res_buffer), np.max([len(ps) for ps in res_buffer])))
-    for i, c in enumerate(res_buffer):
-        res['samples'][i, :len(c)] = c
+            res_ch = scene.computeIR([src], [lis], ctx)
+            rate = res_ch['rate']
+            sa = res_ch['samples'][0][0][0]
+            res['rate'] = rate
+            res_buffer.append(sa)
+        res_temp = np.zeros((len(res_buffer), np.max([len(ps) for ps in res_buffer])))
+        for i, c in enumerate(res_buffer):
+             res_temp[i, :len(c)] = c
+        res['samples'].append(res_temp)
+    np.save('/root/data/gsound_rir/rir_' + str(num) + '.npy',res)
     return res
 
+def main(num):
+    phi1 = random.randint(0,210)
+    deta_phi = random.randint(-60,60)
+    phi2 = int(phi1 + deta_phi)
+    mic_centerx = random.randint(5,31)
+    mic_centery = random.randint(5,31)
+    distance1 = round(random.uniform(1.00,1.40),2)
+    distance2 = round(random.uniform(1.00,1.40),2)
+    x_s1 = mic_centerx+distance1*np.cos(np.deg2rad(phi1))
+    y_s1 = mic_centery+distance1*np.sin(np.deg2rad(phi1))
+    x_s2 = mic_centerx+distance2*np.cos(np.deg2rad(phi2))
+    y_s2 = mic_centery+distance2*np.sin(np.deg2rad(phi2))
+    spk1_pos = np.array([x_s1, y_s1, 1.70])
+    spk2_pos = np.array([x_s2, y_s2, 1.70])
+    spk_pos = np.c_[spk1_pos,spk2_pos]
 
-def main():
-    custom_array = np.array(
-        [[0, 0.035, 0], [-0.0303, 0.0175, 0], [-0.0303, -0.0175, 0], [0, -0.035, 0], [0.0303, -0.0175, 0],
-         [0.0303, 0.0175, 0]])
+    # src_coord = [x_s1, y_s1, 1.7]
+    lis_coord = [mic_centerx, mic_centery, 1.7]
 
-    src_coord = [1, 1, 1]
-    lis_coord = [0.1, 0.1, 0.1]
+    res = compute_array(mesh2, spk_pos, lis_coord, custom_array,num)
 
-    res = compute_array("cube.obj", src_coord, lis_coord, 0.5, 0.1, custom_array)
-
-    with WaveWriter('custom_array.wav', channels=np.shape(res['samples'])[0], samplerate=int(res['rate'])) as w:
-        w.write(np.array(res['samples']))
-
+    # with WaveWriter('array_rir0.wav', channels=np.shape(res['samples'][0])[0], samplerate=int(res['rate'])) as w:
+    #     w.write(np.array(res['samples'][0]))
 
 if __name__ == '__main__':
-    main()
+    mp.set_start_method('spawn')
+
+    cpu_num = 2
+    train_idx = list(range(0,30000))
+    # main(0)
+    with Pool(cpu_num) as p:
+        r = list(tqdm(p.imap(main, train_idx), total=len(train_idx)))
+
