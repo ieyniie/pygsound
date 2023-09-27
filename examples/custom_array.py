2788895bca5289def259ebfcc4c2a08e33b300ca
Xiao Yongxiong <xiaoyx@zhejianglab.com>
2023-09-27 05:37:53 +0000

    custom array

--- examples/custom_array.py
+++ examples/custom_array.py
@@ -10,11 +10,11 @@ from tqdm import tqdm
 custom_array = np.array(
 [[0, 0.035, 0], [-0.0303, 0.0175, 0], [-0.0303, -0.0175, 0], [0, -0.035, 0], [0.0303, -0.0175, 0],
     [0.0303, 0.0175, 0]])
-r = 0.5
+r = 0.6
 sound_a = np.array([0.5,0.6,0.6,0.7,0.75,0.8,0.9,0.9])
 s = 0.1
 # mesh1 = ps.loadobj("cube.obj",r,s)
-mesh2 = ps.createbox(36, 50, 8, sound_a, s)
+mesh2 = ps.createbox(20, 30, 6, sound_a, s)
 def compute_array(mesh, src_coord, lis_coord, micarray,num):
     ctx = ps.Context()
     ctx.diffuse_count = 20000
@@ -32,7 +32,7 @@ def compute_array(mesh, src_coord, lis_coord, micarray,num):
         res_buffer = []
         for offset in micarray:
             lis = ps.Listener((offset + lis_coord).tolist())
-            lis.radius = 0.001
+            lis.radius = 0.002
 
             res_ch = scene.computeIR([src], [lis], ctx)
             rate = res_ch['rate']
@@ -43,17 +43,17 @@ def compute_array(mesh, src_coord, lis_coord, micarray,num):
         for i, c in enumerate(res_buffer):
              res_temp[i, :len(c)] = c
         res['samples'].append(res_temp)
-    np.save('/root/data/gsound_rir/rir_' + str(num) + '.npy',res)
+    np.save('/root/data/gsound_rir2/rir_' + str(num) + '.npy',res)
     return res
 
 def main(num):
     phi1 = random.randint(0,210)
     deta_phi = random.randint(-60,60)
     phi2 = int(phi1 + deta_phi)
-    mic_centerx = random.randint(5,31)
-    mic_centery = random.randint(5,31)
-    distance1 = round(random.uniform(1.00,1.40),2)
-    distance2 = round(random.uniform(1.00,1.40),2)
+    mic_centerx = random.randint(5,15)
+    mic_centery = random.randint(5,15)
+    distance1 = round(random.uniform(1.00,1.30),2)
+    distance2 = round(random.uniform(1.00,1.30),2)
     x_s1 = mic_centerx+distance1*np.cos(np.deg2rad(phi1))
     y_s1 = mic_centery+distance1*np.sin(np.deg2rad(phi1))
     x_s2 = mic_centerx+distance2*np.cos(np.deg2rad(phi2))
@@ -63,7 +63,7 @@ def main(num):
     spk_pos = np.c_[spk1_pos,spk2_pos]
 
     # src_coord = [x_s1, y_s1, 1.7]
-    lis_coord = [mic_centerx, mic_centery, 1.7]
+    lis_coord = [mic_centerx, mic_centery, 1.68]
 
     res = compute_array(mesh2, spk_pos, lis_coord, custom_array,num)
 
@@ -73,7 +73,7 @@ def main(num):
 if __name__ == '__main__':
     mp.set_start_method('spawn')
 
-    cpu_num = 2
+    cpu_num = 24
     train_idx = list(range(0,30000))
     # main(0)
     with Pool(cpu_num) as p:
