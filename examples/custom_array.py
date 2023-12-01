fc25adb7918ac788a862b1513f0244ab6e6b4dd0
xiaoyx_tianshu <xiaoyx@zhejianglab.com>
2023-12-01 14:17:22 +0800

    source height,distance,room size changing

--- examples/custom_array.py
+++ examples/custom_array.py
@@ -14,7 +14,7 @@ r = 0.6
 sound_a = np.array([0.5,0.6,0.6,0.7,0.75,0.8,0.9,0.9])
 s = 0.1
 # mesh1 = ps.loadobj("cube.obj",r,s)
-mesh2 = ps.createbox(20, 30, 6, sound_a, s)
+mesh2 = ps.createbox(40, 50, 10, sound_a, s)
 def compute_array(mesh, src_coord, lis_coord, micarray,num):
     ctx = ps.Context()
     ctx.diffuse_count = 20000
@@ -43,23 +43,25 @@ def compute_array(mesh, src_coord, lis_coord, micarray,num):
         for i, c in enumerate(res_buffer):
              res_temp[i, :len(c)] = c
         res['samples'].append(res_temp)
-    np.save('/root/data/gsound_rir2/rir_' + str(num) + '.npy',res)
+    np.save('/data/datasets/gsound_rir3/rir_' + str(num) + '.npy',res)
     return res
 
 def main(num):
     phi1 = random.randint(0,210)
     deta_phi = random.randint(-60,60)
     phi2 = int(phi1 + deta_phi)
-    mic_centerx = random.randint(5,15)
-    mic_centery = random.randint(5,15)
-    distance1 = round(random.uniform(1.00,1.30),2)
-    distance2 = round(random.uniform(1.00,1.30),2)
+    mic_centerx = random.randint(3,20)
+    mic_centery = random.randint(3,20)
+    distance1 = round(random.uniform(0.05,1.40),2)
+    distance2 = round(random.uniform(0.05,1.40),2)
     x_s1 = mic_centerx+distance1*np.cos(np.deg2rad(phi1))
     y_s1 = mic_centery+distance1*np.sin(np.deg2rad(phi1))
     x_s2 = mic_centerx+distance2*np.cos(np.deg2rad(phi2))
     y_s2 = mic_centery+distance2*np.sin(np.deg2rad(phi2))
-    spk1_pos = np.array([x_s1, y_s1, 1.70])
-    spk2_pos = np.array([x_s2, y_s2, 1.70])
+    h1 = round(random.uniform(1.50,1.80),2)
+    h2 = round(random.uniform(1.50,1.80),2)
+    spk1_pos = np.array([x_s1, y_s1, h1])
+    spk2_pos = np.array([x_s2, y_s2, h2])
     spk_pos = np.c_[spk1_pos,spk2_pos]
 
     # src_coord = [x_s1, y_s1, 1.7]
@@ -73,7 +75,7 @@ def main(num):
 if __name__ == '__main__':
     mp.set_start_method('spawn')
 
-    cpu_num = 24
+    cpu_num = 16
     train_idx = list(range(0,30000))
     # main(0)
     with Pool(cpu_num) as p:
